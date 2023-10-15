fd85e0b9294bd165d58b8deed67e62d36b0b87c0
apexdev <mwangi.em37@gmail.com>
2023-10-15 10:24:53 +0300

    feat: Add solution 1.1
    Complete and verify the solution to exercise 1.1.
    
    - Add script with solution
    - Add README for basic section info.
    - Format script with "isort" and "black".

---
+++ apexDev37/solutions/section-01/README.md
@@ -0,0 +1,13 @@
+### Section 1
+## Python Review
+> Overview: A very fast-paced review of Python
+
+### Core Objectives
+
+- Running Python
+- Interactive Mode
+- Creating Programs
+- Running Programs
+
+### Excercise 1.1
+Read exercise description [here](../../../Exercises/ex1_1.md).
---
+++ apexDev37/solutions/section-01/art.py
@@ -0,0 +1,22 @@
+"""
+Solution for exercise 1.1, section 1.
+
+Module: art.py
+"""
+
+
+import random
+import sys
+
+chars = "\|/"
+
+
+def draw(rows, columns):
+    for r in range(rows):
+        print("".join(random.choice(chars) for _ in range(columns)))
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 3:
+        raise SystemExit("Usage: art.py <rows: int> <columns: int>")
+    draw(int(sys.argv[1]), int(sys.argv[2]))
